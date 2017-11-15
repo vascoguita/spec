0f61b0e289c25a9e223d71dcdb48e8840c642175
Federico Vaga <federico.vaga@vaga.pv.it>
2017-11-15 16:26:26 +0100

    kernel: configure the GenNum chip
    Signed-off-by: Federico Vaga <federico.vaga@vaga.pv.it>

--- kernel/spec-core.c
+++ kernel/spec-core.c
@@ -26,6 +26,67 @@
 #define SPEC_FLAG_BITS (8)
 
 
+/* Registers for GN4124 access */
+enum {
+	/* page 106 */
+	GNPPCI_MSI_CONTROL	= 0x48,		/* actually, 3 smaller regs */
+	GNPPCI_MSI_ADDRESS_LOW	= 0x4c,
+	GNPPCI_MSI_ADDRESS_HIGH	= 0x50,
+	GNPPCI_MSI_DATA		= 0x54,
+
+	GNPCI_SYS_CFG_SYSTEM	= 0x800,
+
+	/* page 130 ff */
+	GNINT_CTRL		= 0x810,
+	GNINT_STAT		= 0x814,
+	GNINT_CFG_0		= 0x820,
+	GNINT_CFG_1		= 0x824,
+	GNINT_CFG_2		= 0x828,
+	GNINT_CFG_3		= 0x82c,
+	GNINT_CFG_4		= 0x830,
+	GNINT_CFG_5		= 0x834,
+	GNINT_CFG_6		= 0x838,
+	GNINT_CFG_7		= 0x83c,
+#define GNINT_CFG(x) (GNINT_CFG_0 + 4 * (x))
+
+	/* page 146 ff */
+	GNGPIO_BASE = 0xA00,
+	GNGPIO_BYPASS_MODE	= GNGPIO_BASE,
+	GNGPIO_DIRECTION_MODE	= GNGPIO_BASE + 0x04, /* 0 == output */
+	GNGPIO_OUTPUT_ENABLE	= GNGPIO_BASE + 0x08,
+	GNGPIO_OUTPUT_VALUE	= GNGPIO_BASE + 0x0C,
+	GNGPIO_INPUT_VALUE	= GNGPIO_BASE + 0x10,
+	GNGPIO_INT_MASK		= GNGPIO_BASE + 0x14, /* 1 == disabled */
+	GNGPIO_INT_MASK_CLR	= GNGPIO_BASE + 0x18, /* irq enable */
+	GNGPIO_INT_MASK_SET	= GNGPIO_BASE + 0x1C, /* irq disable */
+	GNGPIO_INT_STATUS	= GNGPIO_BASE + 0x20,
+	GNGPIO_INT_TYPE		= GNGPIO_BASE + 0x24, /* 1 == level */
+	GNGPIO_INT_VALUE	= GNGPIO_BASE + 0x28, /* 1 == high/rise */
+	GNGPIO_INT_ON_ANY	= GNGPIO_BASE + 0x2C, /* both edges */
+
+	/* page 158 ff */
+	FCL_BASE		= 0xB00,
+	FCL_CTRL		= FCL_BASE,
+	FCL_STATUS		= FCL_BASE + 0x04,
+	FCL_IODATA_IN		= FCL_BASE + 0x08,
+	FCL_IODATA_OUT		= FCL_BASE + 0x0C,
+	FCL_EN			= FCL_BASE + 0x10,
+	FCL_TIMER_0		= FCL_BASE + 0x14,
+	FCL_TIMER_1		= FCL_BASE + 0x18,
+	FCL_CLK_DIV		= FCL_BASE + 0x1C,
+	FCL_IRQ			= FCL_BASE + 0x20,
+	FCL_TIMER_CTRL		= FCL_BASE + 0x24,
+	FCL_IM			= FCL_BASE + 0x28,
+	FCL_TIMER2_0		= FCL_BASE + 0x2C,
+	FCL_TIMER2_1		= FCL_BASE + 0x30,
+	FCL_DBG_STS		= FCL_BASE + 0x34,
+
+	FCL_FIFO		= 0xE00,
+
+	PCI_SYS_CFG_SYSTEM	= 0x800
+};
+
+
 static DECLARE_BITMAP(spec_minors, SPEC_MINOR_MAX);
 static dev_t basedev;
 static struct class *spec_class;
@@ -46,6 +107,64 @@ struct spec_dev {
 	DECLARE_BITMAP(flags, SPEC_FLAG_BITS);
 	void __iomem *remap[3];	/* ioremap of bar 0, 2, 4 */
 };
+
+
+/**
+ * It reads a 32bit register from the gennum chip
+ * @spec spec device instance
+ * @reg gennum register offset
+ * Return: a 32bit value
+ */
+static inline uint32_t gennum_readl(struct spec_dev *spec, int reg)
+{
+	return readl(spec->remap[2] + reg);
+}
+
+
+/**
+ * It writes a 32bit register to the gennum chip
+ * @spec spec device instance
+ * @val a 32bit valure
+ * @reg gennum register offset
+ */
+static inline void gennum_writel(struct spec_dev *spec, uint32_t val, int reg)
+{
+	writel(val, spec->remap[2] + reg);
+}
+
+
+/**
+ * It writes a 32bit register to the gennum chip according to the given mask
+ * @spec spec device instance
+ * @mask bit mask of the bits to actually write
+ * @val a 32bit valure
+ * @reg gennum register offset
+ */
+static inline void gennum_mask_val(struct spec_dev *spec,
+				   uint32_t mask, uint32_t val, int reg)
+{
+	uint32_t v = gennum_readl(spec, reg);
+	v &= ~mask;
+	v |= val;
+	gennum_writel(spec, v, reg);
+}
+
+
+/**
+ * It configures the gennum chip
+ * @spec spec device instance
+ * Return: 0 on success, otherwise a negative errno number
+ */
+static int gennum_config(struct spec_dev *spec)
+{
+	/* Put our 6 pins to a sane state (4 test points, 2 from FPGA) */
+	gennum_mask_val(spec, 0xfc0, 0x000, GNGPIO_BYPASS_MODE); /* no AF */
+	gennum_mask_val(spec, 0xfc0, 0xfc0, GNGPIO_DIRECTION_MODE); /* input */
+	gennum_writel(spec, 0xffff, GNGPIO_INT_MASK_SET); /* disable */
+
+	return 0;
+}
+
 
 
 /**
@@ -228,8 +347,14 @@ static int spec_probe(struct pci_dev *pdev,
 		goto err_enable;
 	pci_set_master(pdev);
 
+	err = gennum_config(spec);
+	if (err)
+		goto err_gennum;
+
 	return 0;
 
+err_gennum:
+	pci_disable_device(pdev);
 err_enable:
 	pci_set_drvdata(pdev, NULL);
 	device_unregister(&spec->dev);
