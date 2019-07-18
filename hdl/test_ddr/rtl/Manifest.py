8dc21b4aa42f2450c5f62bb30d5c1b0ffeb74f2a
Dimitris Lampridis <dimitris.lampridis@cern.ch>
2019-07-18 09:24:22 +0200

    [hdl] remove obsolete test_ddr project

--- hdl/test_ddr/rtl/Manifest.py
+++
@@ -1,9 +0,0 @@
-files = ["spec_ddr_test.vhd",
-         "gpio_regs.vhd"]
-
-modules = {"svn" : ["http://svn.ohwr.org/gn4124-core/trunk/hdl/gn4124core/rtl",
-                    "http://svn.ohwr.org/gn4124-core/trunk/hdl/common/rtl",
-                    "http://svn.ohwr.org/ddr3-sp6-core/trunk/hdl"],
-           "git" : "git://ohwr.org/hdl-core-lib/general-cores.git"}
-
-fetchto = "../ip_cores"
