6fc64042956c478dbd02d63da43fe0fe97ac48e9
Federico Vaga <federico.vaga@cern.ch>
2019-07-19 12:03:35 +0200

    sw:drv: swap cap mask DMA-BLD
    Signed-off-by: Federico Vaga <federico.vaga@cern.ch>

--- software/kernel/spec-core-fpga.c
+++ software/kernel/spec-core-fpga.c
@@ -70,8 +70,8 @@ enum spec_fpga_meta_cap_mask {
 	SPEC_META_CAP_THERM = BIT(1),
 	SPEC_META_CAP_SPI = BIT(2),
 	SPEC_META_CAP_WR = BIT(3),
-	SPEC_META_CAP_DMA = BIT(4),
-	SPEC_META_CAP_BLD = BIT(5),
+	SPEC_META_CAP_BLD = BIT(4),
+	SPEC_META_CAP_DMA = BIT(5),
 };
 
 
