00a51e440be3ea8228805064963af0902de91528
Federico Vaga <federico.vaga@cern.ch>
2020-06-26 11:59:16 +0200

    tst: fix test_dma_read parameters
    Signed-off-by: Federico Vaga <federico.vaga@cern.ch>

--- pytest/test_dma.py
+++ pytest/test_dma.py
@@ -25,11 +25,19 @@ class TestDma(object):
         spec.dma_stop()
 
     @pytest.mark.parametrize("dma_size",
-                             [random.randrange(0, 1 * 1024 * 1024, 4) for i in range(100)])
+                             [0, 1, 2, 3] + \
+                             [5, 7, 9] + \
+                             [2**i for i in range(24)])
     def test_dma_read(self, spec, dma_size):
         """
-        Read from SPEC DDR at random offsets. We just want to see if the DMA
-        engine reports errors
+        We just want to see if the DMA engine reports errors. Test the
+        engine with different sizes, but same offset (default:
+        0x0). On the engine side we will get several transfers
+        (scatterlist) depending on the size.
+
+        Regressions:
+        - 0: the driver returns immediatly without starting any DMA transfer
+        - 1, 2, 3, 4: performing transfers of these sizes led to a failure
         """
         spec.dma_start()
         data = spec.dma_read(dma_size)
