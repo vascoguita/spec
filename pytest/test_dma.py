239dde569540110e2ad06b6ec0b2aa32b9ec0a17
Federico Vaga <federico.vaga@cern.ch>
2020-06-30 08:40:57 +0200

    tst: regressions are actually true limitations, remove test
    Signed-off-by: Federico Vaga <federico.vaga@cern.ch>

--- pytest/test_dma.py
+++ pytest/test_dma.py
@@ -24,48 +24,36 @@ class TestDma(object):
             spec_c.dma_start()
         spec.dma_stop()
 
-    @pytest.mark.parametrize("dma_size",
-                             [0, 1, 2, 3] + \
-                             [5, 7, 9] + \
-                             [2**i for i in range(24)])
-    def test_dma_read(self, spec, dma_size):
+    @pytest.mark.parametrize("buffer_size",
+                             [2**i for i in range(3, 22)])
+    def test_dma_read(self, spec, buffer_size):
         """
         We just want to see if the DMA engine reports errors. Test the
         engine with different sizes, but same offset (default:
         0x0). On the engine side we will get several transfers
         (scatterlist) depending on the size.
-
-        Regressions:
-        - 0: the driver returns immediatly without starting any DMA transfer
-        - 1, 2, 3, 4: performing transfers of these sizes led to a failure
         """
         spec.dma_start()
-        data1 = spec.dma_read(0, dma_size)
-        assert len(data1) == dma_size
-        data2 = spec.dma_read(0, dma_size)
-        assert len(data2) == dma_size
+        data1 = spec.dma_read(0, buffer_size)
+        data2 = spec.dma_read(0, buffer_size)
         spec.dma_stop()
 
+        assert len(data1) == buffer_size
+        assert len(data2) == buffer_size
         assert data1 == data2
 
-    @pytest.mark.parametrize("dma_size",
-                             [0, 1, 2, 3] + \
-                             [5, 7, 9] + \
-                             [2**i for i in range(24)])
-    def test_dma_write(self, spec, dma_size):
+    @pytest.mark.parametrize("buffer_size",
+                             [2**i for i in range(3, 22)])
+    def test_dma_write(self, spec, buffer_size):
         """
         We just want to see if the DMA engine reports errors. Test the
         engine with different sizes, but same offset (default:
         0x0). On the engine side we will get several transfers
         (scatterlist) depending on the size.
-
-        Regressions:
-        - 0: the driver returns immediatly without starting any DMA transfer
-        - 1, 2, 3, 4: performing transfers of these sizes led to a failure
         """
         spec.dma_start()
-        spec.dma_write(0, b"\x00" * dma_size)
-        assert count == dma_size
+        count = spec.dma_write(0, b"\x00" * buffer_size)
+        assert count == buffer_size
         spec.dma_stop()
 
 
