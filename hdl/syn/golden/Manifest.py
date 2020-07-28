7051bce9b2730a1015a2d95eae9e15f72595e9ef
Dimitris Lampridis <dimitris.lampridis@cern.ch>
2020-07-28 13:08:44 +0200

    hdl: re-define and clean-up golden to also include DDR3.
    Signed-off-by: Dimitris Lampridis <dimitris.lampridis@cern.ch>

--- hdl/syn/golden/Manifest.py
+++ hdl/syn/golden/Manifest.py
@@ -1,35 +1,33 @@
 target = "xilinx"
 action = "synthesis"
+board  = "spec"
+
+syn_device  = "xc6slx45t"
+syn_grade   = "-3"
+syn_package = "fgg484"
+syn_project = "spec_golden-45T.xise"
+syn_tool    = "ise"
+syn_top     = "spec_golden"
+
+spec_base_ucf = ['onewire', 'spi', 'ddr3']
+
+ctrls = ["bank3_32b_32b" ]
+
+files = [
+  "buildinfo_pkg.vhd",
+]
+
+modules = {
+  "local" : [
+    "../../top/golden",
+    "../../syn/common",
+  ],
+}
 
 # Allow the user to override fetchto using:
 #  hdlmake -p "fetchto='xxx'"
 if locals().get('fetchto', None) is None:
   fetchto = "../../ip_cores"
-
-syn_device = "xc6slx45t"
-syn_grade = "-3"
-syn_package = "fgg484"
-syn_project = "spec_golden.xise"
-syn_tool = "ise"
-syn_top = "spec_golden"
-
-spec_base_ucf = ['onewire', 'spi']
-board = "spec"
-ctrls = ["bank3_64b_32b" ]
-
-files = [ "buildinfo_pkg.vhd" ]
-
-modules = {
-  "local" : [
-      "../../top/golden", "../../syn/common"
-      ],
-  "git" : [
-      "https://ohwr.org/project/wr-cores.git",
-      "https://ohwr.org/project/general-cores.git",
-      "https://ohwr.org/project/gn4124-core.git",
-      "https://ohwr.org/project/ddr3-sp6-core.git",
-  ],
-}
 
 # Do not fail during hdlmake fetch
 try:
--- hdl/top/golden/spec_golden.vhd
+++ hdl/top/golden/spec_golden.vhd
@@ -9,7 +9,7 @@
 -- description: SPEC golden design, without WR.
 --
 --------------------------------------------------------------------------------
--- Copyright CERN 2019
+-- Copyright CERN 2019-2020
 --------------------------------------------------------------------------------
 -- Copyright and related rights are licensed under the Solderpad Hardware
 -- License, Version 2.0 (the "License"); you may not use this file except
@@ -24,132 +24,154 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
-use work.wishbone_pkg.all;
 
 entity spec_golden is
+
+  generic (
+    -- Simulation-mode enable parameter. Set by default (synthesis) to 0, and
+    -- changed to non-zero in the instantiation of the top level DUT in the testbench.
+    -- Its purpose is to reduce some internal counters/timeouts to speed up simulations.
+    g_SIMULATION : boolean := FALSE
+    );
   port (
     -- Global ports
-    clk_125m_pllref_p_i : in std_logic;  -- 125 MHz PLL reference
+    clk_125m_pllref_p_i : in std_logic;
     clk_125m_pllref_n_i : in std_logic;
 
-    gn_RST_N_i : in std_logic;           -- Reset from GN4124 (RSTOUT18_N)
+    -- GN4124
+    gn_rst_n_i      : in    std_logic;
+    gn_p2l_clk_n_i  : in    std_logic;
+    gn_p2l_clk_p_i  : in    std_logic;
+    gn_p2l_rdy_o    : out   std_logic;
+    gn_p2l_dframe_i : in    std_logic;
+    gn_p2l_valid_i  : in    std_logic;
+    gn_p2l_data_i   : in    std_logic_vector(15 downto 0);
+    gn_p_wr_req_i   : in    std_logic_vector(1 downto 0);
+    gn_p_wr_rdy_o   : out   std_logic_vector(1 downto 0);
+    gn_rx_error_o   : out   std_logic;
+    gn_l2p_clk_n_o  : out   std_logic;
+    gn_l2p_clk_p_o  : out   std_logic;
+    gn_l2p_dframe_o : out   std_logic;
+    gn_l2p_valid_o  : out   std_logic;
+    gn_l2p_edb_o    : out   std_logic;
+    gn_l2p_data_o   : out   std_logic_vector(15 downto 0);
+    gn_l2p_rdy_i    : in    std_logic;
+    gn_l_wr_rdy_i   : in    std_logic_vector(1 downto 0);
+    gn_p_rd_d_rdy_i : in    std_logic_vector(1 downto 0);
+    gn_tx_error_i   : in    std_logic;
+    gn_vc_rdy_i     : in    std_logic_vector(1 downto 0);
+    gn_gpio_b       : inout std_logic_vector(1 downto 0);
 
-    -- General Purpose Interface
-    gn_GPIO_b : inout std_logic_vector(1 downto 0);  -- GPIO[0] -> GN4124 GPIO8
-                                                -- GPIO[1] -> GN4124 GPIO9
-    -- PCIe to Local [Inbound Data] - RX
-    gn_P2L_RDY_o    : out std_logic;       -- Rx Buffer Full Flag
-    gn_P2L_CLK_n_i  : in  std_logic;       -- Receiver Source Synchronous Clock-
-    gn_P2L_CLK_p_i  : in  std_logic;       -- Receiver Source Synchronous Clock+
-    gn_P2L_DATA_i   : in  std_logic_vector(15 downto 0);  -- Parallel receive data
-    gn_P2L_DFRAME_i : in  std_logic;       -- Receive Frame
-    gn_P2L_VALID_i  : in  std_logic;       -- Receive Data Valid
-
-    -- Inbound Buffer Request/Status
-    gn_P_WR_REQ_i : in  std_logic_vector(1 downto 0);  -- PCIe Write Request
-    gn_P_WR_RDY_o : out std_logic_vector(1 downto 0);  -- PCIe Write Ready
-    gn_RX_ERROR_o : out std_logic;                     -- Receive Error
-
-    -- Local to Parallel [Outbound Data] - TX
-    gn_L2P_DATA_o   : out std_logic_vector(15 downto 0);  -- Parallel transmit data
-    gn_L2P_DFRAME_o : out std_logic;       -- Transmit Data Frame
-    gn_L2P_VALID_o  : out std_logic;       -- Transmit Data Valid
-    gn_L2P_CLK_n_o  : out std_logic;  -- Transmitter Source Synchronous Clock-
-    gn_L2P_CLK_p_o  : out std_logic;  -- Transmitter Source Synchronous Clock+
-    gn_L2P_EDB_o    : out std_logic;       -- Packet termination and discard
-
-    -- Outbound Buffer Status
-    gn_L2P_RDY_i    : in std_logic;        -- Tx Buffer Full Flag
-    gn_L_WR_RDY_i   : in std_logic_vector(1 downto 0);  -- Local-to-PCIe Write
-    gn_P_RD_D_RDY_i : in std_logic_vector(1 downto 0);  -- PCIe-to-Local Read Response Data Ready
-    gn_TX_ERROR_i   : in std_logic;        -- Transmit Error
-    gn_VC_RDY_i     : in std_logic_vector(1 downto 0);  -- Channel ready
-
-    -- PCB version
-    pcbrev_i : in std_logic_vector(3 downto 0);
-
-    -- Font panel LEDs
---  LED_RED   : out std_logic;
---  LED_GREEN : out std_logic;
-
-    button1_n_i   : in  std_logic;
+    -- PCB version and reset button
+    pcbrev_i    : in std_logic_vector(3 downto 0);
+    button1_n_i : in std_logic;
 
     -- I2C to the FMC
     fmc0_scl_b : inout std_logic;
     fmc0_sda_b : inout std_logic;
 
-    --  FMC presence  (there is a pull-up)
-    fmc0_prsnt_m2c_n_i: in std_logic;
+    -- FMC presence (there is a pull-up)
+    fmc0_prsnt_m2c_n_i : in std_logic;
 
+    -- DDR3
+    ddr_a_o       : out   std_logic_vector(13 downto 0);
+    ddr_ba_o      : out   std_logic_vector(2 downto 0);
+    ddr_cas_n_o   : out   std_logic;
+    ddr_ck_n_o    : out   std_logic;
+    ddr_ck_p_o    : out   std_logic;
+    ddr_cke_o     : out   std_logic;
+    ddr_dq_b      : inout std_logic_vector(15 downto 0);
+    ddr_ldm_o     : out   std_logic;
+    ddr_ldqs_n_b  : inout std_logic;
+    ddr_ldqs_p_b  : inout std_logic;
+    ddr_odt_o     : out   std_logic;
+    ddr_ras_n_o   : out   std_logic;
+    ddr_reset_n_o : out   std_logic;
+    ddr_rzq_b     : inout std_logic;
+    ddr_udm_o     : out   std_logic;
+    ddr_udqs_n_b  : inout std_logic;
+    ddr_udqs_p_b  : inout std_logic;
+    ddr_we_n_o    : out   std_logic;
+
+    -- Onewire
     onewire_b : inout std_logic;
 
+    -- SPI
     spi_sclk_o : out std_logic;
     spi_ncs_o  : out std_logic;
     spi_mosi_o : out std_logic;
     spi_miso_i : in  std_logic
     );
+
 end spec_golden;
 
-architecture rtl of spec_golden is
-  signal clk_sys_62m5  : std_logic;
-  signal rst_sys_62m5_n  : std_logic;
+architecture arch of spec_golden is
 
-  signal gn_wb_out         : t_wishbone_master_out;
-  signal gn_wb_in          : t_wishbone_master_in;
 begin
-  inst_spec_base: entity work.spec_base_wr
+
+  inst_spec_base : entity work.spec_base_wr
     generic map (
-      g_WITH_VIC     => True,
-      g_WITH_ONEWIRE => True,
-      g_WITH_SPI     => True,
-      g_WITH_DDR     => False,
-      g_WITH_WR      => False,
-      g_simulation   => False
-    )
+      g_WITH_VIC      => TRUE,
+      g_WITH_ONEWIRE  => TRUE,
+      g_WITH_SPI      => TRUE,
+      g_WITH_DDR      => TRUE,
+      g_DDR_DATA_SIZE => 32,
+      g_WITH_WR       => FALSE,
+      g_SIMULATION    => g_SIMULATION
+      )
     port map (
       clk_125m_pllref_p_i => clk_125m_pllref_p_i,
       clk_125m_pllref_n_i => clk_125m_pllref_n_i,
-      gn_rst_n_i => gn_rst_n_i,
-      gn_p2l_clk_n_i => gn_p2l_clk_n_i,
-      gn_p2l_clk_p_i => gn_p2l_clk_p_i,
-      gn_p2l_rdy_o => gn_p2l_rdy_o,
-      gn_p2l_dframe_i => gn_p2l_dframe_i,
-      gn_p2l_valid_i => gn_p2l_valid_i,
-      gn_p2l_data_i => gn_p2l_data_i,
-      gn_p_wr_req_i => gn_p_wr_req_i,
-      gn_p_wr_rdy_o => gn_p_wr_rdy_o,
-      gn_rx_error_o => gn_rx_error_o,
-      gn_l2p_clk_n_o => gn_l2p_clk_n_o,
-      gn_l2p_clk_p_o => gn_l2p_clk_p_o,
-      gn_l2p_dframe_o => gn_l2p_dframe_o,
-      gn_l2p_valid_o => gn_l2p_valid_o,
-      gn_l2p_edb_o => gn_l2p_edb_o,
-      gn_l2p_data_o => gn_l2p_data_o,
-      gn_l2p_rdy_i => gn_l2p_rdy_i,
-      gn_l_wr_rdy_i => gn_l_wr_rdy_i,
-      gn_p_rd_d_rdy_i => gn_p_rd_d_rdy_i,
-      gn_tx_error_i => gn_tx_error_i,
-      gn_vc_rdy_i => gn_vc_rdy_i,
-      gn_gpio_b => gn_gpio_b,
-      fmc0_scl_b => fmc0_scl_b,
-      fmc0_sda_b => fmc0_sda_b,
-      fmc0_prsnt_m2c_n_i => fmc0_prsnt_m2c_n_i,
-      onewire_b => onewire_b,
-      spi_sclk_o => spi_sclk_o,
-      spi_ncs_o => spi_ncs_o,
-      spi_mosi_o => spi_mosi_o,
-      spi_miso_i => spi_miso_i,
-      pcbrev_i => pcbrev_i,
+      gn_rst_n_i          => gn_rst_n_i,
+      gn_p2l_clk_n_i      => gn_p2l_clk_n_i,
+      gn_p2l_clk_p_i      => gn_p2l_clk_p_i,
+      gn_p2l_rdy_o        => gn_p2l_rdy_o,
+      gn_p2l_dframe_i     => gn_p2l_dframe_i,
+      gn_p2l_valid_i      => gn_p2l_valid_i,
+      gn_p2l_data_i       => gn_p2l_data_i,
+      gn_p_wr_req_i       => gn_p_wr_req_i,
+      gn_p_wr_rdy_o       => gn_p_wr_rdy_o,
+      gn_rx_error_o       => gn_rx_error_o,
+      gn_l2p_clk_n_o      => gn_l2p_clk_n_o,
+      gn_l2p_clk_p_o      => gn_l2p_clk_p_o,
+      gn_l2p_dframe_o     => gn_l2p_dframe_o,
+      gn_l2p_valid_o      => gn_l2p_valid_o,
+      gn_l2p_edb_o        => gn_l2p_edb_o,
+      gn_l2p_data_o       => gn_l2p_data_o,
+      gn_l2p_rdy_i        => gn_l2p_rdy_i,
+      gn_l_wr_rdy_i       => gn_l_wr_rdy_i,
+      gn_p_rd_d_rdy_i     => gn_p_rd_d_rdy_i,
+      gn_tx_error_i       => gn_tx_error_i,
+      gn_vc_rdy_i         => gn_vc_rdy_i,
+      gn_gpio_b           => gn_gpio_b,
+      fmc0_scl_b          => fmc0_scl_b,
+      fmc0_sda_b          => fmc0_sda_b,
+      fmc0_prsnt_m2c_n_i  => fmc0_prsnt_m2c_n_i,
+      onewire_b           => onewire_b,
+      spi_sclk_o          => spi_sclk_o,
+      spi_ncs_o           => spi_ncs_o,
+      spi_mosi_o          => spi_mosi_o,
+      spi_miso_i          => spi_miso_i,
+      pcbrev_i            => pcbrev_i,
+      button1_n_i         => button1_n_i,
+      ddr_a_o             => ddr_a_o,
+      ddr_ba_o            => ddr_ba_o,
+      ddr_cas_n_o         => ddr_cas_n_o,
+      ddr_ck_n_o          => ddr_ck_n_o,
+      ddr_ck_p_o          => ddr_ck_p_o,
+      ddr_cke_o           => ddr_cke_o,
+      ddr_dq_b            => ddr_dq_b,
+      ddr_ldm_o           => ddr_ldm_o,
+      ddr_ldqs_n_b        => ddr_ldqs_n_b,
+      ddr_ldqs_p_b        => ddr_ldqs_p_b,
+      ddr_odt_o           => ddr_odt_o,
+      ddr_ras_n_o         => ddr_ras_n_o,
+      ddr_reset_n_o       => ddr_reset_n_o,
+      ddr_rzq_b           => ddr_rzq_b,
+      ddr_udm_o           => ddr_udm_o,
+      ddr_udqs_n_b        => ddr_udqs_n_b,
+      ddr_udqs_p_b        => ddr_udqs_p_b,
+      ddr_we_n_o          => ddr_we_n_o
+      );
 
-      ddr_dma_clk_i  => clk_sys_62m5,
-      ddr_dma_rst_n_i => rst_sys_62m5_n,
-
-      clk_62m5_sys_o    => clk_sys_62m5,
-      rst_62m5_sys_n_o  => rst_sys_62m5_n,
-
-      --  Everything is handled by the carrier.
-      app_wb_o         => gn_wb_out,
-      app_wb_i         => gn_wb_in
-    );
-    gn_wb_in <= (ack => '1', err | rty | stall => '0', dat => (others => '0'));
-end rtl;
+end architecture arch;
