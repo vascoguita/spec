63bdb675aa3af5070181d99269e211976dfdfb81
Vaibhav Gupta <vaibhav.gupta@cern.ch>
2022-11-08 15:57:11 +0100

    kernel: spec-core: Use sysfs instead of debugfs
    Along with SPEC devices, we also have SVEC devices which have symmetric
    code. Due to kernel upgrade, SVEC started using sysfs to get the name of
    firmware to be loaded.
    Thus, to keep the code symmetric, we should do similar change in SPEC.
    
    Signed-off-by: Vaibhav Gupta <vaibhav.gupta@cern.ch>

--- software/kernel/spec-core.c
+++ software/kernel/spec-core.c
@@ -72,37 +72,20 @@ static const struct file_operations spec_irq_dbg_info_ops = {
 };
 
 #define SPEC_DBG_FW_BUF_LEN 128
-static ssize_t spec_dbg_fw_write(struct file *file,
-				 const char __user *buf,
-				 size_t count, loff_t *ppos)
+static ssize_t spec_fpga_firmware_store(struct device *dev,
+					struct device_attribute *attr,
+					const char *buf,
+					size_t count)
 {
-	struct spec_gn412x *spec_gn412x = file->private_data;
-	char buf_l[SPEC_DBG_FW_BUF_LEN];
+	struct pci_dev *pdev = to_pci_dev(dev);
+	struct spec_gn412x *spec_gn412x = pci_get_drvdata(pdev);
 	int err;
 
-	if (SPEC_DBG_FW_BUF_LEN < count) {
-		dev_err(&spec_gn412x->pdev->dev,
-			 "Firmware name too long max %u\n",
-			SPEC_DBG_FW_BUF_LEN);
-
-		return -EINVAL;
-	}
-	memset(buf_l, 0, SPEC_DBG_FW_BUF_LEN);
-	err = copy_from_user(buf_l, buf, count);
-	if (err)
-		return -EFAULT;
-
-	err = spec_fw_load(spec_gn412x, buf_l);
-	if (err)
-		return err;
-	return count;
+	err = spec_fw_load(spec_gn412x, buf);
+	return err ? err : count;
 }
 
-static const struct file_operations spec_dbg_fw_ops = {
-	.owner = THIS_MODULE,
-	.open  = simple_open,
-	.write = spec_dbg_fw_write,
-};
+static DEVICE_ATTR_WO(spec_fpga_firmware);
 
 static void seq_printf_meta(struct seq_file *s, const char *indent,
 			    struct spec_meta_id *meta)
@@ -197,16 +180,6 @@ static int spec_dbg_init(struct spec_gn412x *spec_gn412x)
 		dev_err(dev, "Cannot create debugfs file \"%s\" (%ld)\n",
 			SPEC_DBG_INFO_NAME, PTR_ERR(spec_gn412x->dbg_info));
 		return PTR_ERR(spec_gn412x->dbg_info);
-	}
-
-	spec_gn412x->dbg_fw = debugfs_create_file(SPEC_DBG_FW_NAME, 0200,
-						  spec_gn412x->dbg_dir,
-						  spec_gn412x,
-						  &spec_dbg_fw_ops);
-	if (IS_ERR_OR_NULL(spec_gn412x->dbg_fw)) {
-		dev_err(dev, "Cannot create debugfs file \"%s\" (%ld)\n",
-			SPEC_DBG_FW_NAME, PTR_ERR(spec_gn412x->dbg_fw));
-		return PTR_ERR(spec_gn412x->dbg_fw);
 	}
 
 	spec_gn412x->dbg_meta = debugfs_create_file(SPEC_DBG_META_NAME, 0200,
@@ -665,6 +638,7 @@ static DEVICE_ATTR_WO(load_golden_fpga);
 
 static struct attribute *gn412x_fpga_attrs[] = {
 	&dev_attr_load_golden_fpga.attr,
+	&dev_attr_spec_fpga_firmware.attr,
 	&dev_attr_bootselect.attr,
 	NULL,
 };
